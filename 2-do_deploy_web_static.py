d9dfe42d7358389e935c8729c293e61047ab54ce
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2024-02-01 04:03:42 -0800

    commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,44 +4,27 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from invoke import run, task
+from fabric.api import put, run, env
 from os.path import exists
+env.hosts = ['54.167.183.175', '100.26.169.40']
 
-env = {'host_string': '54.167.183.175,100.26.169.40'}
 
-@task
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
-
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        
-        # Upload the archive to /tmp/
-        run(f'put {archive_path} /tmp/')
-
-        # Create the release directory
-        run(f'mkdir -p {path}{no_ext}/')
-
-        # Extract the archive to the release directory
-        run(f'tar -xzf /tmp/{file_n} -C {path}{no_ext}/')
-
-        # Remove the uploaded archive
-        run(f'rm /tmp/{file_n}')
-
-        # Move the contents to the parent directory and clean up
-        run(f'mv {path}{no_ext}/web_static/* {path}{no_ext}/')
-        run(f'rm -rf {path}{no_ext}/web_static')
-
-        # Update the symbolic link to the current release
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s {path}{no_ext}/ /data/web_static/current')
-
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
-        print(f"Error: {e}")
+    except:
         return False
-
